374eb320b1d9e3c4a933fe04a0f06de60420a71a
AnthonyR <arankin789@gmail.com>
2022-03-21 10:17:37 -0400

    Form captures field values

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,23 @@ var createTaskHandler = function(event){
     // Prevents the window from refreshing the page.  
     event.preventDefault();
 
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // Creates a list item with class of 'task-item'
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task";
+    
+    // Creates a div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+
+    // Add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class = 'task-name'>" + taskNameInput + "</h3><span class = 'task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    // Add entire list item to list
     taskToDoEl.appendChild(listItemEl);
 };
 
